e8d0e5d08669cb4258e2adf5206c6cfd66d8b0f1
Dark Tailed <dark.tailed.wolf@gmail.com>
2013-08-27 21:40:48 -0700

    Finished upgrading the language files.

--- Fur.py
+++ Fur.py
@@ -35,11 +35,6 @@ except: pass
 	#os.console('cls')
 	#os.console('clear')
 
-sandwich = Food('sandwich', 'A ham and cheese on rye bread.', False)
-p1MainRoom = Room('Main Room', items = [sandwich],doors = [Door('northwest', 'Balcony'), Door('northeast', 'Hallway'), Door('southeast', 'Play Room'), Door('southwest', 'Chest Room', key="Bronze")])
-p1Balcony = Room('Balcony', items = [], doors=[Door('south', 'Main Room')])
-p1Hallway = Room('Hallway', items = [], doors=[Door('south', 'Main Room'), Door('west','Garden', key="A")])
-
 def quit(msg, nosave=False):
 	global ios, devplayer
 	if nosave: pass
@@ -204,7 +199,7 @@ def start():
 
 #part 1 init
 def part1():
-	global loc,gender,player_name,player_last,frnd_gender,frnd_nane,frnd_last,devplayer
+	global map,gender,player_name,player_last,frnd_gender,frnd_nane,frnd_last,devplayer,player,friend
 	playing = True
 	output('p1m1', s=2)
 	output('p1m2', s=2)
@@ -229,6 +224,8 @@ def part1():
 		i = False
 		if frnd_name: waiting = False
 		else: output('nofriendnameerror', r=1, addon=(frnd_gndrpn, frnd_gender))
+	player = Character(loc, player_name, player_last, species, gender, 1)
+	friend = Character(loc, frnd_nane, player_last, 'Fox', frnd_gender, 1)
 	output('p1m12', addon=frnd_name, s=2)
 	output('p1m13', s=1)
 	output('p1m14', addon=player_name, s=2)
@@ -246,87 +243,73 @@ def part1():
 		elif player_species == output('bear', r=1): temp += output('beardesc', r=1)
 		elif player_species == output('mouse', r=1): temp += output('mousedesc', r=1)
 		elif player_species == output('otter', r=1): temp += output('otterdesc', r=1)
-	output(83, addon=temp)
-	time.sleep(0.5)
-	output(93)
-	time.sleep(3)
-	output(94, addon=frnd_name)
-	time.sleep(1)
-	output(95, addon=player_species)
-	time.sleep(2)
-	output(96)
-	time.sleep(2)
-	output(97)
-	time.sleep(1)
-	output(98, addon=[frnd_name, player_species, frnd_gndrpn])
-	time.sleep(3)
-	output(99)
-	time.sleep(2)
-	output(100, addon=frnd_name)
-	time.sleep(1)
-	output(101)
-	time.sleep(5)
+	output('p1m17', addon=temp, s=0.5)
+	output('p1m19', s=3)
+	output('p1m20', addon=frnd_name, s=1)
+	output('p1m21', addon=player_species, s=2)
+	output('p1m22', s=2)
+	output('p1m23', s=1)
+	output('p1m24', addon=(frnd_name, player_species, frnd_gndrpn), s=3)
+	output('p1m25', s=2)
+	output('p1m26', addon=frnd_name, s=1)
+	output('p1m27', s=5)
 	output('')
 	color('blue')
-	output(102)
+	output('p1title', s=2)
 	color('reset')
 	output('')
-	time.sleep(2)
-	loc = 'p1MainRoom'
+	map = 'part1'
 	gameplay()
 
 def gameplay():
-	global loc, p1MainRoom, p1RoomTwo, devplayer, player, gender, frnd_gender, player_name, player_last, player_species, frnd_gndrpn, species, frnd_nane
-	player = Character(loc, player_name, player_last, species, gender, 1)
-	friend = Character(loc, frnd_nane, player_last, 'Fox', frnd_gender, 1)
+	global loc, p1MainRoom, p1RoomTwo, devplayer, player, gender, friend, frnd_gender, player_name, player_last, player_species, frnd_gndrpn, species, frnd_nane, map
 	playing=True
-	if not devplayer: quit(output(103, r=1))
-	output(104)
-	if loc == 'p1MainRoom':
-		location = p1MainRoom
-		location.describe()
+	quit(output('broken4', r=1))
+	output('gamestart')
 	playing=True
 	while playing:
 		output("")
-		cmnd = getCommand(getInput.text(output(105, r=1)))
+		cmnd = getCommand(getInput.text(output('gameaction', r=1)))
 		if not cmnd:
-			output(106)
+			output('gameinputerror')
 			continue
-		if cmnd['verb'] == output(107, r=1): quit(output(109))
-		elif cmnd['verb'] == output(110, r=1):
+		if cmnd['verb'] == output('quitcmd', r=1): quit(output('quitmsg', r=1))
+		elif cmnd['verb'] == output('savecmd', r=1):
 			save()
 			continue
-		elif cmnd['verb'] == output(112, r=1):
+		elif cmnd['verb'] == output('loadcmd', r=1):
 			load()
 			continue
-		elif cmnd['verb'] == output(113, r=1):
-			output(108)
-			output(111)
-			output(114)
-			output(116)
-			output(118)
-			output(120)
-			output(123)
+		elif cmnd['verb'] == output('helpcmd', r=1):
+			output('helpquit')
+			output('helpsave')
+			output('helpload')
+			output('helphelp')
+			output('helpexamine')
+			output('helpeat')
+			output('helpdrink')
+			output('helptake')
+			output('helpgo')
 			continue
 		noun = cmnd.get('noun')
 		if not noun: continue
 		else:
 			item = location.findItem(noun)
-			if cmnd['verb'] == output(115, r=1) and hasattr(item, 'examine'): item.examine()
-			elif cmnd['verb'] == output(117, r=1) and hasattr(item, 'eat'): item.eat()
-			elif cmnd['verb'] == output(119, r=1) and hasattr(item, 'drink'): item.drink()
-			elif cmnd['verb'] == output(121, r=1) and hasattr(player, 'take'): player.take(item)
+			if cmnd['verb'] == output('examinecmd', r=1) and hasattr(item, 'examine'): item.examine()
+			elif cmnd['verb'] == output('eatcmd', r=1) and hasattr(item, 'eat'): item.eat()
+			elif cmnd['verb'] == output('drinkcmd', r=1) and hasattr(item, 'drink'): item.drink()
+			elif cmnd['verb'] == output('takecmd', r=1) and hasattr(player, 'take'): player.take(item)
 			elif not item:
-				if cmnd['verb'] == output(122, r=1):
+				if cmnd['verb'] == output('gocmd', r=1):
 					roomname = location.go(noun)
-					if not roomname: output(124)
-					elif roomname == "locked": output(125)
-					elif roomname == "invalid": output(126)
+					if not roomname: output('doormissing')
+					elif roomname == "locked": output('doorlocked')
+					elif roomname == "invalid": output('directionerror')
 					else:
 						location = rooms[roomname]
 						location.describe()
-				else: output(127, addon=noun)
-			else: output(128)
+				else: output('itemerror', addon=noun)
+			else: output('cmderror')
 
 def init():
 	global ios, pc
