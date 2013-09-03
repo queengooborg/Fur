51d781b9699cc601e5da289d52dfe088ea9e5ba5
Dark Tailed <dark.tailed.wolf@gmail.com>
2013-09-03 13:18:34 -0700

    Started on battle.py

---
+++ battle.py
@@ -0,0 +1,24 @@
+# Move
+# Attack
+# Items
+# Magic
+# Skill
+# Run
+
+def move():
+	pass
+
+def attack():
+	pass
+
+def items():
+	pass
+
+def magic():
+	pass
+
+def skill():
+	pass
+
+def run():
+	pass
